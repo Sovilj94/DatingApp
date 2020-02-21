e7115f1678f039485da442dd58b8cdb30b04a089
Nikola Sovilj <nikola.sovilj@lanaco.com>
2020-02-21 14:17:08 +0100

    End of modul 5

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -19,7 +19,7 @@ export class NavComponent implements OnInit {
     this.authService.login(this.model).subscribe(next => {
       console.log('Logged in Succefuly');
     }, error => {
-      console.log('Failed while logging');
+      console.log(error);
     });
   }
 
